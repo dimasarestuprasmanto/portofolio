83340e5095a1156591631f734350217c0bcee3bf
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2021-11-03 14:40:21 +0700

    Membuat Page Qualification

--- assets/js/utama.js
+++ assets/js/utama.js
@@ -52,7 +52,26 @@ skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContents =>{
+            tabContents.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
